28a29f7ff4bfaa6a5bfbea93516cf689689651bd
Maria Sanzhanova <smo_0207@mail.ru>
2022-09-18 19:04:53 +0500

    Conflict_resolved

--- src/Main.java
+++ src/Main.java
@@ -42,9 +42,6 @@ public class Main {
         boolean doBonus = sum >= MIN_COST_FOR_BONUS;
         for (int i = 0; i < products.length; i++) {
             if (counts[i] != 0) {
-
-                System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * counts[i]) + " руб.");
-
                 boolean isOnSale = false;
                 for (String saleProduct : productsOnSale) {
                     if (products[i].equals(saleProduct)) {
